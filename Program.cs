c3f6c6fb9e830fd5103933d6159868fa99193d34
zelya2021 <43150758+zelya2021@users.noreply.github.com>
2019-04-02 00:01:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Примеры_C_шарп
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0, k;
+            int num;
+            do
+            {
+                k = 0;
+                Console.Clear();
+                Console.WriteLine("Введите целое положительное число: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num < 0)
+                {
+                    k++;
+                }
+
+            } while (k != 0);
+
+            while (num != 0)
+            {
+                s = s * 10 + num % 10;
+                num /= 10;
+            }
+
+            Console.WriteLine($"Перевернутое число: {s}");
+
+        }
+
+    }
+}
